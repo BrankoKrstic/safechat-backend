6e1cb3b9759a03662dce3a81f1fcc90fd556912b
BrankoKrstic <brankokrstic94@gmail.com>
2021-07-05 20:17:40 +0200

    filter rooms more carefully

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ function getActiveRooms(io) {
 	// Filter rooms whose name exist in set:
 	// ==> [['room1', Set(2)], ['room2', Set(2)]]
 	const filtered = arr.filter(
-		(room) => !room[1].has(room[0]) && !regexExp.test(room[1])
+		(room) =>
+			!room[1].has(room[0]) &&
+			!regexExp.test(room[1]) &&
+			regexExp.test(room[0])
 	);
 	// Return only the room name:
 	// ==> ['room1', 'room2']
