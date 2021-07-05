70325533972ce1f5de62df26a6a09d88cc3d9539
BrankoKrstic <brankokrstic94@gmail.com>
2021-07-05 20:25:03 +0200

    fix filtering error

--- index.js
+++ index.js
@@ -13,19 +13,10 @@ const regexExp =
 	/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
 
 function getActiveRooms(io) {
-	// Convert map into 2D list:
-	// ==> [['4ziBKG9XFS06NdtVAAAH', Set(1)], ['room1', Set(2)], ...]
 	const arr = Array.from(io.sockets.adapter.rooms);
-	// Filter rooms whose name exist in set:
-	// ==> [['room1', Set(2)], ['room2', Set(2)]]
 	const filtered = arr.filter(
-		(room) =>
-			!room[1].has(room[0]) &&
-			!regexExp.test(room[1]) &&
-			regexExp.test(room[0])
+		(room) => !room[1].has(room[0]) && !regexExp.test(room[1])
 	);
-	// Return only the room name:
-	// ==> ['room1', 'room2']
 	const res = filtered.map((i) => i[0]);
 	return res;
 }
